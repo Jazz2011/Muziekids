3e6c25634b4ae3eea6ac98f897314c886b84a7a9
Jazz Belarbi <jazzbelarbi2011@gmail.com>
2024-11-29 20:53:24 +0100

    Initial Commit

---
+++ MuziekidsTests/MuziekidsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MuziekidsTests.swift
+//  MuziekidsTests
+//
+//  Created by Jazz Belarbi on 29/11/2024.
+//
+
+import Testing
+@testable import Muziekids
+
+struct MuziekidsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
